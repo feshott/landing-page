7efe40516e4a817812ba34467c2ae090c43d2d6c
Yuri Feshott <feshot@yandex.ru>
2017-08-18 14:04:56 +0300

    add scrollRevial

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ $(function () {
   });
 });
 
-// ------------------------------CANVAS
 
-//------------------------------CANVAS
+
+// ------------------------------Scroll Revial
+
+window.sr = ScrollReveal({ reset: false,
+                          
+                          distance: '200px',
+
+                         });
+
+// Customizing a reveal set
+sr.reveal('.services-header,.skill-header,.work-header,.contact-header,.feedback-header,.contact-form', { duration: 900 }
+         
+         );
+
+
+
